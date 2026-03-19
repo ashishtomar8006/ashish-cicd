fd96f1c37c0ac196dc31d24fa09f1c62dea97ca0
ashishtomar8006 <ashishtomar.liangtuang@gmail.com>
2026-03-19 13:20:16 +0530

    quotes commit

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import type { Application, Request, Response } from "express";
 import "dotenv/config";
 import cors from "cors";
+
 const app: Application = express();
 const PORT = process.env.PORT || 7000;
 
@@ -10,13 +11,26 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+const quotes: string[] = [
+  "Success is the sum of small efforts repeated day in and day out.",
+  "Do something today that your future self will thank you for.",
+  "Great things never come from comfort zones.",
+  "Push yourself, because no one else is going to do it for you.",
+  "Dream big. Start small. Act now.",
+];
+
 app.get("/", (req: Request, res: Response) => {
-  return res.send("It's working 🙌");
+  return res.send("Hi, welcome to the Express TypeScript API!");
 });
 
+app.get("/api/quotes/random", (req: Request, res: Response) => {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  const randomQuote = quotes[randomIndex];
 
-
+  return res.status(200).json({
+    success: true,
+    quote: randomQuote,
+  });
+});
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
-
-
